a6a7e741d1057f4828f7fbc67c9ee3e5f61358ca
Maxime Pierre <maxime_pierre@homedepot.com>
2020-10-28 21:23:24 -0400

    adding in jquery and making ajax call to craigslist

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
+declare var jQuery: any;
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -9,6 +11,8 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 export class SearchComponent implements OnInit {
 
   searchForm: FormGroup;
+
+  searchPath = '/search/sss?query=cars&sort=rel';
 
   constructor(private fb: FormBuilder) {
     this.searchForm = this.fb.group({
@@ -20,7 +24,11 @@ export class SearchComponent implements OnInit {
   }
 
   submitSearch() {
-    console.log('>> submit search!!!')
+    const searchTerm = encodeURI(this.searchForm.controls['searchTerm'].value);
+
+    jQuery.get(`/search/sss?query=${searchTerm}&sort=rel`, ( data ) => {
+      console.log( data );
+    });
   }
 
 }
