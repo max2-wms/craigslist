fe1aecf91030b755e1f4ddd06853f50066424bb0
Maxime Pierre <maxime_pierre@homedepot.com>
2020-10-28 20:16:34 -0400

    basic routing and adding in bootstrap library

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Components
+import { SearchComponent } from "./components/search/search.component";
+import { SearchResultsComponent } from "./components/search-results/search-results.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SearchComponent
+  },
+  {
+    path: 'search-results',
+    component: SearchResultsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    SearchResultsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
